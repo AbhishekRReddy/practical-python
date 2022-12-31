1b221e648d2531efffbac38aa572259393609e90
AbhishekRReddy <arreddy1997@gmail.com>
2022-12-31 18:21:44 +0530

    Exercise 2.6: Dictionaries as a container

--- Work/report.py
+++ Work/report.py
@@ -8,6 +8,24 @@ def read_portfolio(filename):
         rows=csv.reader(file)
         header=next(rows)
         for row in rows:
-            holding=(row[0],int(row[1]),float(row[2]))
+            holding={}
+            holding['name']=row[0]
+            holding['shares']=row[1]
+            holding['price']=row[2]
             portfolio.append(holding)
         return portfolio
+
+def read_prices(filename):
+    prices={}
+    file=open(filename,'rt')
+    rows=csv.reader(file)
+    header=next(rows)
+    for row in rows:
+        try:
+            prices[row[0]]=float(row[1])       
+        except IndexError:
+            print('Found empty line')
+        
+    return prices
+total_prices=read_prices('Work/Data/prices.csv')
+print(total_prices)
