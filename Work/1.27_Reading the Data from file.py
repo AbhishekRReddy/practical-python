1a254844b2176474b7be247435fdebbe65dc7545
AbhishekRReddy <arreddy1997@gmail.com>
2022-12-31 11:09:08 +0530

    Making the code as Function for more generic use case

--- Work/1.27_Reading the Data from file.py
+++ Work/1.27_Reading the Data from file.py
@@ -1,8 +1,11 @@
-file=open('Work/Data/portfolio.csv','rt')
-headers=next(file).split(',')
-total_cost=0
-for line in file:
-    row=line.split(',')
-    total_cost+=int(row[1])*float(row[2])
-print(f'Total Cost: {total_cost:0.2f}')
-print(row)
+def pcost(filename):
+    file=open(filename,'rt')
+    headers=next(file).split(',')
+    total_cost=0
+    for line in file:
+        row=line.split(',')
+        total_cost+=int(row[1])*float(row[2])
+    return total_cost
+
+cost=pcost('Work/Data/portfolio.csv')
+print(f'Total Cost: {cost:0.2f}')
