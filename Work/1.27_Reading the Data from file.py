5df54fed7de6d4a3c78a5a9ed3ae1c76fe777f21
AbhishekRReddy <arreddy1997@gmail.com>
2022-12-31 12:19:26 +0530

    Made changes in function so that it can get the path of the file from command line

--- Work/1.27_Reading the Data from file.py
+++ Work/1.27_Reading the Data from file.py
@@ -1,11 +1,20 @@
+import csv
+import sys
 def pcost(filename):
-    file=open(filename,'rt')
-    headers=next(file).split(',')
+    file=open(filename)
+    rows=csv.reader(file)
+    headers=next(rows)
     total_cost=0
-    for line in file:
-        row=line.split(',')
-        total_cost+=int(row[1])*float(row[2])
+    for row in rows:
+        try:
+            total_cost+=int(row[1])*float(row[2])
+        except ValueError:
+            print('Invalid Line.....Skipping')
     return total_cost
+if(len(sys.argv)==2):
+    filename=sys.argv[1]
+else:
+    filename='Work/Data/portfolio.csv'
 
-cost=pcost('Work/Data/portfolio.csv')
+cost=pcost(filename)
 print(f'Total Cost: {cost:0.2f}')
