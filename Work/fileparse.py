d4871c2e256d0f9c831279545d428f160f9f9ad3
AbhishekRReddy <arreddy1997@gmail.com>
2023-01-05 19:16:27 +0530

    Made changes to include optional argument for column selector

--- Work/fileparse.py
+++ Work/fileparse.py
@@ -1,15 +1,20 @@
 # fileparse.py
 #
 # Exercise 3.3
-def parse_csv(filename):
+def parse_csv(filename,select=None):
     import csv
     with open(filename) as f:
         rows=csv.reader(f)
         headers=next(rows)
+        if(select):
+            positions=[headers.index(column) for column in select]
+            headers=select
         records=[]
         for row in rows:
             if(not row):
                 continue
+            if(select):
+                row=[row[i] for i in positions]
             record=dict(zip(headers,row))
             records.append(record)
     return records
