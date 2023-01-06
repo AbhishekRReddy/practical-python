449059a52ca7f7c35781d8967787f73342c05087
AbhishekRReddy <arreddy1997@gmail.com>
2023-01-06 19:27:15 +0530

    Made pcost module to use the read_portfolio function of report module to get the list of dictionaries of all the stocks

--- Work/pcost.py
+++ Work/pcost.py
@@ -1,3 +1,14 @@
-# pcost.py
-#
-# Exercise 1.27
+import csv
+import sys
+def pcost(filename):
+    import report
+    portfolio=report.read_portfolio(filename)
+    total_cost=0
+    for i,stock in enumerate(portfolio,start=1):
+        try:
+            share=int(stock['shares'])
+            price=float(stock['price'])
+            total_cost+=share*price
+        except ValueError:
+            print(f'Row {i}:Couldnt convert:{stock}')
+    return total_cost
