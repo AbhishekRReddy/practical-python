18d74b54f44f0a7d1c9e4058ffd9e57f576ca953
AbhishekRReddy <arreddy1997@gmail.com>
2023-01-07 20:21:37 +0530

    created a new class called table formatter which is basically abstract base class to print the headers and row data

---
+++ Work/table_format.py
@@ -0,0 +1,12 @@
+class TableFormatter:
+    def headings(self,headers):
+        '''
+        print the table headers
+        '''
+        raise NotImplementedError
+    def row(self,rowdata):
+        '''
+        Print the single row of the table data
+        '''
+        raise NotImplementedError
+    
