998506e9437a3b6acd71043f8b565366927361b4
AbhishekRReddy <arreddy1997@gmail.com>
2023-01-08 19:35:20 +0530

    Included HTMLTableFormatter

--- Work/table_format.py
+++ Work/table_format.py
@@ -30,3 +30,12 @@ class CSVTableFormatter(TableFormatter):
         print(','.join(headers))
     def row(self, rowdata):
         print(','.join(rowdata))
+
+class HTMLTableFormatter(TableFormatter):
+    '''
+    Output portfolio data in HTML format
+    '''
+    def headings(self, headers):
+        print('<tr><th>'+'</th><th>'.join(headers)+'</th></tr>')
+    def row(self, rowdata):
+        print('<tr><td>'+'</td><td>'.join(rowdata)+'</td></tr>')
